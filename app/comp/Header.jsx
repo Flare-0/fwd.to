d4aba916822bf236bc6ee3c4f71c747f768a8595
flaredash0 <muajkarim@outlook.com>
2024-08-08 19:23:15 +0600

    e

---
+++ app/comp/Header.jsx
@@ -0,0 +1,12 @@
+// Header.js
+"use client";
+
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      
+    </header>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import { initializeApp } from "firebase/app";
+import Header from "./comp/Header.jsx"
 export default function Home() {
+
+  
   return (
   <>
-  
-  
+  <Header/>
   </>
   );
 }
