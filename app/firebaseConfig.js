be86aaca80e1e9a4ebc28b51f162e5e77ef86fe3
flaredash0 <muajkarim@outlook.com>
2024-08-12 18:09:19 +0600

    save

---
+++ app/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiKnuNVC5SwzdXkizEB0MNEmeC1sOxu-E",
+    authDomain: "fwd-to.firebaseapp.com",
+    projectId: "fwd-to",
+    storageBucket: "fwd-to.appspot.com",
+    messagingSenderId: "1038029241453",
+    appId: "1:1038029241453:web:f1f2a9fd67015c1c0fd364"
+  };
+  export { firebaseConfig}
